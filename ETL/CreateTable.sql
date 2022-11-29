823994010c9b19b58d50ec00f06ce66354a64d0d
csubodh <subodhchintawar@gmail.com>
2022-11-28 19:03:32 -0500

    ETL resources

---
+++ ETL/CreateTable.sql
@@ -0,0 +1,26 @@
+
+CREATE TABLE IF NOT EXISTS diabetes_data (
+   Diabetes_012 DECIMAL(3, 1),
+   HighBP DECIMAL(3, 1),
+   HighChol DECIMAL(3, 1),
+   CholCheck DECIMAL(3, 1),
+   BMI DECIMAL(3, 1),
+   Smoker DECIMAL(3, 1),
+   Stroke DECIMAL(3, 1),
+   HeartDiseaseorAttack DECIMAL(3, 1),
+   PhysActivity DECIMAL(3, 1),
+   Fruits DECIMAL(3, 1),
+   Veggies DECIMAL(3, 1),
+   HvyAlcoholConsump DECIMAL(3, 1),
+   AnyHealthcare DECIMAL(3, 1),
+   NoDocbcCost DECIMAL(3, 1),
+   GenHlth DECIMAL(3, 1),
+   MentHlth DECIMAL(3, 1),
+   PhysHlth DECIMAL(3, 1),
+   DiffWalk DECIMAL(3, 1),
+   Sex DECIMAL(3, 1),
+   Age DECIMAL(3, 1),	
+   Education DECIMAL(3, 1),	
+   Income DECIMAL(3, 1)	
+);
+
